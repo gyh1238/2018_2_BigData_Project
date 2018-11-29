d6ae177c9aad8e550841bbaf218ef98143e781f0
SeoPaul <m9472757c@gmail.com>
2018-11-29 20:25:53 +0900

    [Update] Version 1.2.2 - Data Preprocessing Error Found. (Original Data Length is equal to Preprocessing Data Length)

--- jibun_to_location_ex.py
+++ jibun_to_location_ex.py
@@ -3,22 +3,30 @@ from mapAPI.transform import jibun_to_location
 import pandas as pd
 
 if __name__ == '__main__':
-    # 스타벅스.xlsx 데이터 전처리
-    raw_data = pd.read_excel("/Users/seopaul/Desktop/2018_2_BigData_Project/data/raw_data/스타벅스.xlsx")
+    # 전체데이터_서울_원본.xlsx 불러오기
+    raw_data = pd.read_excel("/Users/seopaul/Desktop/2018_2_BigData_Project/data/raw_data/전체데이터_서울_원본.xlsx")
     data = raw_data[['소재지전체주소', '사업장명', '영업상태명']]
+
+    # 통합.xlsx 불러오기
+    store = pd.read_excel("/Users/seopaul/Desktop/2018_2_BigData_Project/data/통합.xlsx")
 
     # 지번주소 필터링
     jibun = raw_data['소재지전체주소']
     jibun = jibun_filter(jibun)
-    data = data.copy()
-    data['지번주소'] = jibun
+    print(len(raw_data))
+    print(len(jibun))
+    
+    # 저장
+    store['프렌차이즈_지번_주소_명'] = jibun
+    store.to_excel("/Users/seopaul/Desktop/2018_2_BigData_Project/data/통합.xlsx", index=False)
 
+    '''
     # 지번주소 -> 위도,경도
     location = jibun_to_location(jibun, atype='Google')
-    data = data.copy()
-    data['위도'] = location[0]
-    data['경도'] = location[1]
 
-    # 정리 및 저장
-    data = pd.DataFrame(data, columns=['소재지전체주소', '지번주소', '사업장명', '영업상태명', '위도', '경도'])
-    data.to_excel("/Users/seopaul/Desktop/2018_2_BigData_Project/data/스타벅스.xlsx", index=False)
+    # 저장
+    store['프렌차이즈_위도'] = location[0]
+    store['프렌차이즈_경도'] = location[1]
+    store.to_excel("/Users/seopaul/Desktop/2018_2_BigData_Project/data/통합.xlsx", index=False)
+    '''
+
--- mapAPI/filter.py
+++ mapAPI/filter.py
@@ -5,41 +5,50 @@ import numpy as np
 
 def jibun_filter(data):
     jibun_list = list()
-    element = {'do': "", 'si': "", 'gu': "", 'dong': "", 'bunji': ""}
     for d in data:
         try:
             tmp = d.split(" ")
+            element = {'do': "", 'si': "", 'gu': "", 'dong': "", 'bunji': ""}
         except AttributeError:
-            jibun_list.append(np.nan)
-        else:
-            if tmp[0][-1:] == '도':
-                element['do'] = tmp[0]
-                for t in tmp[1:]:
-                    if t[-1:] == '시' or t[-1:] == '군':
-                        element['si'] = t
-                    elif t[-1:] == '구' or t[-1:] == '면' or t[-1:] == '읍':
-                        element['gu'] = t
-                    elif t[-1:] == '동' or t[-1:] == '리':
-                        element['dong'] = t
-                    elif t[-2:] == '번지':
-                        element['bunji'] = t[:-2]
-                        jibun = element['do'] + " " + element['si'] + " " + element['gu'] + " " + element["dong"] + " " + element["bunji"]
-                        jibun_list.append(jibun)
-                    else:
-                        pass
-            elif tmp[0][-1:] == '시':
-                element['si'] = tmp[0]
-                for t in tmp[1:]:
-                    if t[-1:] == '구':
-                        element['gu'] = t
-                    elif t[-1:] == '동' or t[-1:] == '가':
-                        element['dong'] = t
-                    elif t[-2:] == '번지':
-                        element['bunji'] = t[:-2]
-                        jibun = element['si'] + " " + element['gu'] + " " + element["dong"] + " " + element["bunji"]
-                        jibun_list.append(jibun)
-                    else:
-                        pass
+            jibun_list.append("입력주소없음")
+            continue
+
+        if tmp[0][-1:] == '도':
+            element['do'] = tmp[0]
+            for t in tmp[1:]:
+                if t[-1:] == '시' or t[-1:] == '군':
+                    element['si'] = t
+                elif t[-1:] == '구' or t[-1:] == '면' or t[-1:] == '읍':
+                    element['gu'] = t
+                elif t[-1:] == '동' or t[-1:] == '리':
+                    element['dong'] = t
+                elif t[-2:] == '번지':
+                    element['bunji'] = t[:-2]
+                else:
+                    pass
+            if element['do'] != "" and element['si'] != "" and element['gu'] != "" and element['dong'] != "" and element['bunji'] != "":
+                jibun = " ".join(list(element.values()))
+                jibun_list.append(jibun)
             else:
-                pass
+                jibun_list.append('입력주소오류')
+
+        elif tmp[0][-1:] == '시':
+            element['si'] = tmp[0]
+            for t in tmp[1:]:
+                if t[-1:] == '구':
+                    element['gu'] = t
+                elif t[-1:] == '동' or t[-1:] == '가':
+                    element['dong'] = t
+                elif t[-2:] == '번지':
+                    element['bunji'] = t[:-2]
+                else:
+                    pass
+            if element['si'] != "" and element['gu'] != "" and element['dong'] != "" and element['bunji'] != "":
+                jibun = " ".join(list(element.values())[1:])
+                jibun_list.append(jibun)
+            else:
+                jibun_list.append('입력주소오류')
+
+        else:
+            jibun_list.append('입력주소오류')
     return Series(jibun_list, name='지번주소')
